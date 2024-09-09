c65f563efe565b502fc2c12399b7cc972d2b49d2
Maria Polyakov <mariapolyakov@Marias-MacBook-Pro.local>
2024-09-08 19:55:04 -0500

    push the project to git

---
+++ questions.js
@@ -0,0 +1,8 @@
+const QUESTIONS = [
+  "Do you have any pets? (emotional support/therapy is considered to be no)",
+  "Do you provide income 3x of the rent?",
+  "Is your credit score at least 650?",
+  "Do you smoke?",
+]
+
+const NOT_QUALIFIED = "You are not qualified to rent this property."
